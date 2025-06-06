f4ce52358e65b7c78eb0514b9850c8ed0a04d8c9
Dreamfarar <80091853+Dreamfarar@users.noreply.github.com>
2025-06-06 15:58:48 +0500

    Update gensyn.sh

--- gensyn.sh
+++ gensyn.sh
@@ -13,11 +13,12 @@ NC='\033[0m' # No Color
 
 echo "📦 Updating server..."
 sudo apt update && sudo apt install -y sudo
-sudo npm install -g localtunnel
+
 
 echo "🔧 Installing required packages..."
 sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl wget screen git lsof nano unzip iproute2 build-essential gcc g++
 [ -f cuda.sh ] && rm cuda.sh; curl -o cuda.sh https://raw.githubusercontent.com/zunxbt/gensyn-testnet/main/cuda.sh && chmod +x cuda.sh && . ./cuda.sh
+sudo npm install -g localtunnel
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && source ~/.nvm/nvm.sh && nvm install 20.18.0 && nvm use 20.18.0 && nvm alias default 20.18.0
 echo "🚀 Starting Gensyn script..."
 screen -S gensyn
